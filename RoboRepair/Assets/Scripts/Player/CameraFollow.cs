5ff5127f47eb561c22966ccecca725878dd547e9
StevenMedeot <55761357+StevenMedeot@users.noreply.github.com>
2020-01-31 20:52:22 -0500

    Merge Fix + Asset Removal

---
+++ RoboRepair/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float dampening = 0.8f;
+    public Transform target;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPos = (transform.position * dampening) + (1 - dampening) * target.position;
+        transform.position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
+    }
+}
