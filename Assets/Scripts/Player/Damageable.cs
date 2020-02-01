496fc9c0a5af2be30d01b19955e9e200ed8aece1
Alfred-Langer <>
2020-02-01 16:53:10 -0500

    Hassaan And Alfy's things
    we did a thing

---
+++ Assets/Scripts/Player/Damageable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    [SerializeField]
+    protected SpriteRenderer sprite;
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected IEnumerator Flash()
+    {
+        if(sprite == null)
+        {
+            yield return null;
+        }
+        float flashSeconds = 0.1f;
+        sprite.color = Color.red;
+        yield return new WaitForSeconds(flashSeconds);
+        sprite.color = Color.white;
+        yield return new WaitForSeconds(flashSeconds);
+        sprite.color = Color.red;
+        yield return new WaitForSeconds(flashSeconds);
+        sprite.color = Color.white;
+    }
+}
