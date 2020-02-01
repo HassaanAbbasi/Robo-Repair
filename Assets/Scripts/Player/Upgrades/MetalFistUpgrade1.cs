a4fe88b3262fcb03e70eb90865125f5722c050cd
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2020-02-01 12:06:30 -0500

    Jetpack Upgrade
    - Added changeable number of max jumps

---
+++ Assets/Scripts/Player/Upgrades/MetalFistUpgrade1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : PlayerUpgrade
+{
+    [SerializeField]
+    protected float cooldown = 5.0f;
+    //when player interacts with enemy type" breakable wall" && player pressing use metal fist button, destroy(wall)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
