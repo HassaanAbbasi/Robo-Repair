c3cee450c76aae2d8abda2b430b550dea9e3a096
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2020-02-01 22:24:43 -0500

    But why did you stop

--- Assets/Scripts/Player/Upgrades/MetalFistUpgrade.cs
+++ Assets/Scripts/Player/Upgrades/MetalFistUpgrade.cs
@@ -8,7 +8,10 @@ public class MetalFistUpgrade : PlayerUpgrade
     protected float cooldown = 5.0f;
     [SerializeField]
     protected bool canPunch = true;
+    [SerializeField]
+    protected float wait = 0.1f;
 
+    bool canDestroy = false;
     GameObject fist;
     SpriteRenderer sprender;
     //when player interacts with enemy type" breakable wall" && player pressing use metal fist button, destroy(wall)
@@ -20,25 +23,26 @@ public class MetalFistUpgrade : PlayerUpgrade
         sprender = fist.GetComponent<SpriteRenderer>();
         sprender.enabled = false;
     }
+
     public override void UseUpgrade(int direction)
     {
-        if (Input.GetKeyDown(KeyCode.F) && canPunch)
+        if (Input.GetKeyDown(KeyCode.Q) && canPunch)
         {
-            CheckWallBreak(direction);
             canPunch = false;
+            CheckWallBreak(direction);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void CheckWallBreak(int direction)
     {
         sprender.enabled = true;
-
+        StartCoroutine(Punch(direction));
         //StartCoroutine(CooldownTimer());
     }
 
@@ -46,5 +50,45 @@ public class MetalFistUpgrade : PlayerUpgrade
     {
         yield return new WaitForSeconds(cooldown);
         canPunch = true;
+    }
+
+    public IEnumerator Punch(int direction)
+    {
+        if (direction > 0)
+        {
+            Vector3 pos = fist.transform.position;
+            yield return new WaitForSeconds(wait);
+            fist.transform.position = new Vector3(fist.transform.position.x - 0.1f, fist.transform.position.y, fist.transform.position.z);
+            yield return new WaitForSeconds(wait);
+            fist.transform.position = new Vector3(fist.transform.position.x + 0.3f, fist.transform.position.y, fist.transform.position.z);
+            canDestroy = true;
+            yield return new WaitForSeconds(wait);
+            fist.transform.position = new Vector3(fist.transform.position.x - 0.2f, fist.transform.position.y, fist.transform.position.z);
+            canDestroy = false;
+            sprender.enabled = false;
+            StartCoroutine(CooldownTimer());
+        }
+        else if(direction < 0)
+        {
+            Vector3 pos = fist.transform.position;
+            yield return new WaitForSeconds(wait);
+            fist.transform.position = new Vector3(fist.transform.position.x + 0.1f, fist.transform.position.y, fist.transform.position.z);
+            yield return new WaitForSeconds(wait);
+            fist.transform.position = new Vector3(fist.transform.position.x -0.3f, fist.transform.position.y, fist.transform.position.z);
+            canDestroy = true;
+            yield return new WaitForSeconds(wait);
+            fist.transform.position = new Vector3(fist.transform.position.x + 0.2f, fist.transform.position.y, fist.transform.position.z);
+            canDestroy = false;
+            sprender.enabled = false;
+            StartCoroutine(CooldownTimer());
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "BreakableWall" && canDestroy)
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
