a2c09bb0852473c33a40a25b14aff53e9dbffc17
Alfred-Langer <>
2020-02-02 03:11:39 -0500

    Steven Saved the DAy

--- Assets/Scripts/Enemy/DrillMole.cs
+++ Assets/Scripts/Enemy/DrillMole.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class DrillMole : moleBase
 {
+
+    public enum DrillState
+    {
+        DS_HOP = 0,
+        DS_CHARGE,
+        DS_RETURN,
+        DS_COUNT
+    }
+    
     [SerializeField]
-    protected int hopFlag = 1;
-    [SerializeField]
-    protected int chargeFlag = 0;
+    protected DrillState currentState = DrillState.DS_HOP;
+
     protected SpriteRenderer sprite;
     [SerializeField]
     protected GameObject player;
     [SerializeField]
     protected Vector3 startPosition;
+    [SerializeField]
+    protected int hopDirection = 1;
+
     // Start is called before the first frame update
     protected new void Start()
     {
@@ -25,16 +36,28 @@ public class DrillMole : moleBase
     // Update is called once per frame
     override protected void Update()
     {
-        if (hopFlag != 0)
+
+        switch (currentState)
         {
-            hop();
-            rigidbody.velocity = new Vector3(0, 5 * hopFlag, 0);
-            if (rigidbody.transform.position.x - player.transform.position.x < 8)
-                chargeFlag = 1;
-        }
-        else if (startPosition.x - rigidbody.transform.position.x >= 6)
-        {
-            returnToPosition();
+            case DrillState.DS_HOP:
+                hop();
+                rigidbody.velocity = new Vector3(0, 5 * hopDirection, 0);
+                if (rigidbody.transform.position.x - player.transform.position.x < 6)
+                    currentState++;
+                break;
+
+            case DrillState.DS_CHARGE:
+
+                StartCoroutine(charge());
+
+                break;
+            case DrillState.DS_RETURN:
+
+                 returnToPosition();
+                break;
+            default:
+                Debug.Log("MEH");
+                break;
         }
 
     }
@@ -43,17 +66,15 @@ public class DrillMole : moleBase
     {
         if (rigidbody.transform.position.y >= -0.36)
         {
-            hopFlag = -1;
+            hopDirection = -1;
         }
         else if (rigidbody.transform.position.y <= -0.65)
         {
-            hopFlag = 1;
-            if (chargeFlag == 1)
+            hopDirection = 1;
+            if (currentState == DrillState.DS_CHARGE)
             {
-                hopFlag = 0;
+                hopDirection = 0;
                 rigidbody.velocity = new Vector3(0, rigidbody.transform.position.y, 0);
-                print("meh");
-                StartCoroutine(charge());
             }
         }
         
@@ -62,16 +83,28 @@ public class DrillMole : moleBase
 
     private IEnumerator charge()
     {
-        print("bleh");
-        rigidbody.transform.localScale = new Vector3(rigidbody.transform.localScale.x * -1, 1, 1);
-        yield return new WaitForSeconds(2);
-        rigidbody.velocity = new Vector3(-5, 0, 0);
+        
+        currentState = DrillState.DS_COUNT;
+        //rigidbody.transform.localScale = new Vector3(rigidbody.transform.localScale.x * -1, 1, 1);
+        yield return new WaitForSeconds(1);
+        rigidbody.velocity = new Vector3(-20, 0, 0);
+
+        yield return new WaitForSeconds(0.5f);
+
+        currentState = DrillState.DS_RETURN;
     }
 
     void returnToPosition()
     {
-        //rigidbody.velocity = new Vector3(1, 0, 0);
-        rigidbody.velocity = Vector3.Lerp(rigidbody.transform.position, startPosition, 0);
-        hopFlag = 1;
+
+        rigidbody.velocity = new Vector3(Vector3.Normalize((startPosition - transform.position)).x * 3 , 0, 0);
+
+
+        if (transform.position.x >= startPosition.x - 0.1f && transform.position.x <= startPosition.x + 0.1f)
+        {
+            hopDirection = 1;
+            rigidbody.velocity = new Vector3(0, 0, 0);
+            currentState = 0;
+        }
     }
 }
